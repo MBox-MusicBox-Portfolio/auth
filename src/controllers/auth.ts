7b74ba7e7f6c08c9fb2ffecd3afd3e03907b2ee9
Sergey Zhyltsov <48886027+SergRikhter@users.noreply.github.com>
2024-05-31 05:08:13 +0300

    Modify auth controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -22,10 +22,10 @@ export async function authUser(object:any, ctx:any) : Promise<any>
                     return({success:false, value:"Incorrect password"});       
             }else if(await checkingBlockedUserOrNo(object) == false){
                     ctx.status=200;
-                    const user = await getUserInstance(object); 
-                    const jwt = await token("encrypt",{id: user.dataValues.id, username:user.dataValues.usernames, avatar:user.dataValues.avatars, rolesId: user.dataValues.rolesId});
-                    await redis.RedisSetValue("AuthKey"+jwt,{id: user.dataValues.id, username:user.dataValues.usernames, avatar:user.dataValues.avatars, rolesId: user.dataValues.rolesId})
-                    return {success:true,value:{access_token: jwt}};
+                      const user = await getUserInstance(object); 
+                      const jwt = await generateToken(user);
+                    await createRedisRecord(user,jwt);
+                 return  {success:true,value:{access_token:jwt}} 
             }else{
              ctx.status=403;   
              return {success:false,value:"Sorry your login was banned by admin"}
@@ -43,6 +43,33 @@ export async function authUser(object:any, ctx:any) : Promise<any>
     }
 }
 
+/**
+ * Generated new tokens 
+ */
+async function generateToken(user:any) : Promise<any>
+{
+    try{
+        const jwt = await token("encrypt",{Id: user.dataValues.Id, Username:user.dataValues.Username, Avatar:user.dataValues.Avatar, RoleId: user.dataValues.RoleId});
+       // const isJwt = jwt !== undefined ? {success:true,value:{access_token: jwt}} : undefined;
+        return jwt;
+    }catch(error){
+        return error;
+    }
+}
+
+/**
+ * Created new records in the redis
+ * @param user object
+ */
+async function createRedisRecord(user:any, jwt:string) : Promise<any>
+{
+    try{
+        await redis.RedisSetValue("AuthKey"+jwt,{Id: user.dataValues.Id, Username:user.dataValues.Username, Avatar:user.dataValues.Avatar, RoleId: user.dataValues.RoleId})
+       // return true; 
+    }catch(error){
+      console.debug(error);
+    }
+}
 
 /**
  * Check blocked user
@@ -50,43 +77,54 @@ export async function authUser(object:any, ctx:any) : Promise<any>
  */
 async function checkingBlockedUserOrNo(object:any) :Promise<any>
 {
-    const currentUser = await User.findOne({where:{emails:object.email}})
-    const isBlocked = currentUser?.dataValues.isBlockeds  > 0? true : false;
+    const currentUser = await User.findOne({where:{Email:object.Email}})
+    const isBlocked = currentUser?.dataValues.IsBlocked  == true ? true : false;
     return isBlocked;  
 }
 
 /**
- * 
+ * Return User db instance 
  */
 async function getUserInstance(object:any): Promise<any>
 {
     if(await existUserIntoDb(object) !== false)
     {
-        const user = await User.findOne({where:{emails:object.email}});
+        const user = await User.findOne({where:{Email:object.Email}});
         const isUser = user instanceof User == true ? user : false; 
         return isUser; 
     }else{
-        console.log({success:false,values:"User isn't exist"})
+        return {
+            success:false,
+            values:"User isn't exist"
+        }
     }
 }
 
 /**
- * Ch
+ * Check user exist into db table Users
  * @param object 
- * @returns 
+ * @returns true/false or exception
  */
 async function existUserIntoDb(object:any) : Promise<any>
 {
     try{
-        const user = await User.findOne({where:{emails:object.email}});
+        const user = await User.findOne({where:{Email:object.Email}});
          return user instanceof User == true ?  true: false; 
     }catch(error){
-        return {success:false,value:error};
+        return {
+            success:false,
+            value:error
+        };
     }
 }
 
+/**
+ * Compared password hash
+ * @param object 
+ * @returns true or false
+ */
 async function comparePass(object:any) : Promise<boolean>
 {
-  const user = await User.findOne({where:{emails:object.email}});
-  return bcrypt.compareSync(object.password,user?.dataValues.passwords);
+  const user = await User.findOne({where:{Email:object.Email}});
+  return bcrypt.compareSync(object.Password,user?.dataValues.Password);
 }
