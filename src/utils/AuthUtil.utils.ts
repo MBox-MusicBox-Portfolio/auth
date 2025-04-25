5bda7162880bb28edbbd3af61b78361222c988f3
SergRikhter <enigmat0907@gmail.com>
2025-04-25 21:57:38 +0300

    Refactor AuthUtils: improve readability, reusability, and error handling

--- src/utils/AuthUtil.utils.ts
+++ src/utils/AuthUtil.utils.ts
@@ -1,11 +1,76 @@
-import { authDTO } from '../controllers/dto/dto.auth.controller';
+import * as authDTO from '../controllers/dto/dto.auth.controller';
 import { HttpStatusCode } from './HttpStatusCode.util';
 import bcrypt from 'bcrypt';
 import {token} from '../modules/token.modules';
 import * as redis from '../modules/redis.modules';
 import dotenv from 'dotenv';
+import { Context } from 'koa';
+
 
 dotenv.config();
+
+export function handleFailedValidation(errorContext:any,ctx:Context) : authDTO.IAuthDTO
+{
+    ctx.status = HttpStatusCode.BadRequest;
+    return authDTO.handleValidationError(errorContext);
+}
+    
+export function handleAppValidation(errorContext:any,ctx:Context) : authDTO.IAuthDTO
+{
+    ctx.status = HttpStatusCode.BadRequest;
+    return authDTO.handleAppValidationError(errorContext);
+}
+
+export function handleInternalServerError(errorContext:any,ctx:Context) : authDTO.IAuthDTO
+{
+    ctx.status = HttpStatusCode.InternalServerError;
+    return authDTO.handleAppValidationError(errorContext);
+}
+
+export function handleUnauthorized(errorContext:any,ctx:Context) : authDTO.IAuthDTO
+{
+    ctx.status = HttpStatusCode.Unauthorized;
+    return authDTO.handleAppValidationError(errorContext);
+}
+
+export function handleNotFound(errorContext:any,ctx:Context): authDTO.IAuthDTO{
+    ctx.status=HttpStatusCode.NotFound
+    return authDTO.handleAppValidationError(errorContext);
+}
+
+export function handleSuccessfulRegister(message:string,ctx:Context):authDTO.IAuthDTO
+{
+    ctx.status=HttpStatusCode.Created;
+    return authDTO.handleSuccessMessage(message);
+}
+
+export function handleSuccessLogout(message:string,ctx:Context):authDTO.IAuthDTO
+{
+    ctx.status=HttpStatusCode.OK;
+    return authDTO.handleSuccessMessage(message);
+}
+
+export function generateLinkForEmailVerification(token:string):string
+{
+    const link = "http://"+process.env.HTTP_SERVER +"/api/auth/confirmEmail/"+token;
+    return link;
+}
+
+
+/**
+ * For successfull authorization
+ * @param user 
+ * @param ctx 
+ * @returns 
+ */
+export async function handleSuccessfulAuth(user:any, ctx:Context):Promise<authDTO.IAuthDTO>
+{
+    ctx.status=HttpStatusCode.OK;
+       const jwt = await generateTokenForAuth(user);
+                   await createRedisRecordForAuth(user, jwt);
+      return authDTO.handleAuthSuccess(jwt);
+}
+
 
 export async function generateTokenForEmailVerification(user: any): Promise<any> {
     try{  
@@ -82,76 +147,19 @@ export async function RedisDeleteKey(keyName:string):Promise<any>{
     }
 }
 
-
-export function handleFailedValidation(errorContext:any,ctx:any) : any
-{
-    ctx.status = HttpStatusCode.BadRequest;
-    return authDTO(false,null,errorContext);
-}
-    
-export function handleAppValidation(errorContext:any,ctx:any)
-{
-    ctx.status = HttpStatusCode.BadRequest;
-    return authDTO(false,null,null,null,errorContext);
-}
-
-export function handleInternalServerError(errorContext:any,ctx:any)
-{
-    ctx.status = HttpStatusCode.InternalServerError;
-    return authDTO(false,null,null,errorContext);
-}
-
-export function handleUnauthorized(errorContext:any,ctx:any)
-{
-    ctx.status = HttpStatusCode.Unauthorized;
-    return authDTO(false,null,null,errorContext);
-}
-
-export function handleNotFound(errorContext:any,ctx:any){
-    ctx.status=HttpStatusCode.NotFound
-    return authDTO(false,null,null,errorContext);
-}
-
 /**
- * For successfull authorization
- * @param user 
+ * Sending email verification message
+ * @param newUser 
+ * @param message 
  * @param ctx 
  * @returns 
  */
-export async function handleSuccessfulAuth(user:any, ctx:any):Promise<any>
-{
-    ctx.status=HttpStatusCode.OK;
-       const jwt = await generateTokenForAuth(user);
-                   await createRedisRecordForAuth(user, jwt);
-      return authDTO(true,jwt,null);
-}
-
-export function handleSuccessfulRegister(message:string,ctx:any):any
-{
-    ctx.status=HttpStatusCode.Created;
-
-    return authDTO(true,null,message,null,null);
-}
-
-export function handleSuccessLogout(message:string,ctx:any):any
-{
-    ctx.status=HttpStatusCode.OK;
-    return authDTO(true,null,message,null,null);
-}
-
-export async function sendEmailVerificationMessage(newUser:any,message:string,ctx:any):Promise<any>
+export async function sendEmailVerificationMessage(newUser:any,message:string,ctx:any):Promise<authDTO.IAuthDTO>
 {
     ctx.status=HttpStatusCode.OK;
        const jwtEmailKeyForConfirmation = await generateTokenForEmailVerification(newUser); 
        const redisEmailVerificationKey:string = process.env.REDIS_EMAIL_VERIFICATION_KEY + jwtEmailKeyForConfirmation;
-
-    return authDTO(true,null,message,null,null);
-}
-
-export function generateLinkForEmailVerification(token:string):string
-{
-    const link = "http://"+process.env.HTTP_SERVER +"/api/auth/confirmEmail/"+token;
-    return link;
+    return authDTO.handleSuccessMessage(message);
 }
 
 export const comparePass = async (currentPassword:string, existUserPassword:string):Promise<boolean>=>{
